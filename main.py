98abbdf0a0dfa928194c85ad9b338a7c6518f9ba
Kyuwoong Lee <kwooler@gmail.com>
2023-02-26 21:36:37 +0000

    Update

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-
-print ('Hello World')
+print('Hello World')
 
 num1 = 10
 num2 = 20
-print (num1+num2)
+num1 = input()
+num2 = input()
+print(num1 + num2)
 
-message = 'Computer Literarc 101'
-print (message) 
+message = 'Computer Literarcy 101'
+print(message)
