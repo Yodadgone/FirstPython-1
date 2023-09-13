28e4dd668c5ba7c65d3e01cc93ba07c24325515d
kwooler <kwooler@gmail.com>
2023-09-13 09:50:51 -0700

    Lab2

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ def main():
     print('Hello World')
     
 # This is Lab 2
+# This is Lab 2
+# This is Lab 2
+# This is Lab 2
+# This is Lab 2
+# This is Lab 2
 
 if __name__ == '__main__':
     main()
